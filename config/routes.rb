ad4593ca594f46d4843e5e7f8ce8649a47f1990b
Steven Liu <stevotennis@gmail.com>
2017-07-21 13:09:17 -0700

    4.5: added views and utilized partial forms as local variables for DRY software technique

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
