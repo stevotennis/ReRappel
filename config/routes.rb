6a2fa4b8f5bfd7f5f14bdfdc934186a93900a9bf
Steven Liu <stevotennis@gmail.com>
2017-07-21 21:46:07 -0700

    5.1: debugging heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :products
-  resources :static_pages
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
-  get 'static_pages/landing_page'
+  # get 'static_pages/landing_page'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
