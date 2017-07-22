587a91519d047f457b6b2ee09432aba6009e015b
Steven Liu <stevotennis@gmail.com>
2017-07-21 21:35:07 -0700

    5.1: debugging heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  get 'static_pages#landing_page'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+  # root 'static_pages#index'
 
-  #root 'static_pages#landing_page'
+  root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
