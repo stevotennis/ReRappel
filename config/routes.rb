f04198b06e478cea26592cd8cfb2208313527440
Steven Liu <stevotennis@gmail.com>
2017-07-25 20:45:16 -0700

    5.3: added email forms

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  get 'static_pages/thank_you'
+
+  post 'static_pages/thank_you'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'static_pages#index'
