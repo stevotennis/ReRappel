ed89641c526a6b008b1e5ba10fb1fba4189fb2ad
Steven Liu <stevotennis@gmail.com>
2017-07-30 13:21:00 -0700

    5.7: added comment model and display onto producut show page.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :products
+	has_many :comments
 
 	def self.search(search_term)
 	  if Rails.env.development?
@@ -7,5 +8,13 @@ class Product < ApplicationRecord
 	  else
 		Product.where("name ilike ?", "%#{search_term}%")
 	  end
+	end
+
+	def highest_rating_comment
+  	  comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+  	  comments.rating_desc.last
 	end
 end
